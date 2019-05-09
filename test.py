3911504d35f2b0b34c59db85954243736a96fa91
Verina Cristie <cristie_verina@sutd.edu.sg>
2019-05-09 23:34:16 +0800

    add subtract function and execute

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+def substract(a,b):
+    result = float(a) - float(b)
+    print("substract: ", result)
+
 # Store input numbers
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 print("num1: ", num1, " num2: ", num2)
+substract(num1, num2)
